3255a80fc05eae7e87628e50a6ca97ed6a00fb42
yuvraj-bytes <upadhyayyuvi@gmail.com>
2024-05-09 08:53:26 +0530

    Add: Navbar

---
+++ app/(browse)/_components/navbar/logo.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+});
+
+export const Logo = () => {
+  return (
+    <Link href="/">
+      <div className="flex items-center gap-x-4 hover:opacity-75 transition">
+        <div className={cn("hidden lg:block", font.className)}>
+          <p className="text-lg font-semibold">Skill Plus</p>
+        </div>
+      </div>
+    </Link>
+  );
+};
---
+++ app/(browse)/layout.tsx
@@ -0,0 +1,11 @@
+import { Navbar } from "./_components/navbar";
+
+const BrowseLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
+
+export default BrowseLayout;
